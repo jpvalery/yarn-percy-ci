bbe78ed166535dfa9e2285c3aef3ed2ffd302ac2
jpvalery <jp.valery@me.com>
2019-10-08 16:47:31 -0400

    modifygin entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,20 +5,23 @@ set -e
 
 # Requires PERCY_TOKEN
 if [ -n "$PERCY_TOKEN" ]; then
-  echo "## Percy Token not specified. Action will fail."
+    echo "## Percy Token not specified. Action will fail."
 else
+    echo "## Percy Token specified. Continuing..."
 fi
 
 # Setup node modules if needed
 if [ -e node_modules/.bin/percy ]; then
-    echo "## Environment ready."
+    echo "## Environment ready. Continuing..."
 else
     echo "## Environment not ready. Installing modules..."
     sh -c "yarn install"
 fi
 
+# Build with Yarn
 echo "## Building with Yarn..."
 sh -c "yarn build"
 
+# Snapshot with Percy
 echo "## Snapshotting with Percy..."
 sh -c "yarn percy snapshot"
