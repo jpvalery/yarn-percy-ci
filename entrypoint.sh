3d0c7f6538fc2865c9d6224a6afa656b05349b1b
jpvalery <jpvalery@users.noreply.github.com>
2019-10-11 18:59:17 -0400

    Another test on chown omg

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@
 set -e
 
 # Gives the circleci user access to all files and folders
-sh -c "sudo chown -R circleci /github/home"
-sh -c "sudo chmod -R 777 /github/home"
+sh -c "sudo chown -R circleci /github"
+sh -c "sudo chmod -R 777 /github"
 
 # Requires PERCY_TOKEN
 if [ -n "$PERCY_TOKEN" ]; then
